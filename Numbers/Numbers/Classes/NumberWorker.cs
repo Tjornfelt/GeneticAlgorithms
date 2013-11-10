d9fde3b77a21d1c9c45f093900a02099e8489f26
Mads <madstjornfelt@gmail.com>
2013-11-10 23:09:37 +0100

    initial commit

---
+++ Numbers/Numbers/Classes/NumberWorker.cs
@@ -0,0 +1,14 @@
+﻿using Numbers.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers.Classes
+{
+    class NumberWorker:WorkerInterface
+    {
+        public int Number { get; set; }
+    }
+}
---
+++ Numbers/Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using Numbers.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+    class Program
+    {
+        /*
+        exercise: Write a genetic algorithm that calculates the
+        smallest numbers added together to make a certian value.
+        Tricky part: each number can only be used ONCE!
+        */
+
+        const int goal = 100;
+        const int maxRuns = 50;
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < maxRuns; i++)
+            {
+                NumberWorker Worker = new NumberWorker();
+                Worker.Number = i;
+            }
+        }
+    }
+}
